a875ae77f6a1ca0e652b899305f5d7dfaa1cdea6
carterballow <cartsballow@gmail.com>
2025-02-27 18:23:59 -0800

    Half react portfolio
    almost there

---
+++ src/components/Heroimg.js
@@ -0,0 +1,24 @@
+import "./HeroimgStyles.css";
+import React from 'react';
+import backgroundImg from "../assets/background.jpg";
+import { Link } from "react-router-dom";
+
+const HeroImg = () => {
+  return (
+    <div className="hero">
+      <div className="mask">
+        <img className="background-img" src={backgroundImg} alt="background" />
+      </div>
+      <div className="content">
+        <p>HI, I'M A FREELANCER</p>
+        <h1>React Developer.</h1>
+        <div>
+          <Link to="/projects" className="btn">Projects</Link>
+          <Link to="/contact" className="btn btn-light">Contact</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HeroImg;
---
+++ src/routes/contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from "../components/Navbar";
+import Heroimg2 from "../components/Heroimg2";
+import Footer from "../components/Footer";
+const contact = () => {
+  return (
+    <div>
+       <Navbar/>
+        <Heroimg2 heading="CONTACT." text="Feel free to reach out to me."/>
+       <Footer/>
+    </div>
+  )
+}
+
+export default contact
