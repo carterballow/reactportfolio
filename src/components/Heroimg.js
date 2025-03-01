8ee53527aa3911f02f4ff5c2d28e4bf82edeee39
carterballow <cartsballow@gmail.com>
2025-03-01 01:37:55 -0800

    iamcarterballow

--- src/components/Heroimg.js
+++ src/components/Heroimg.js
@@ -11,7 +11,7 @@ const HeroImg = () => {
       </div>
       <div className="content">
         <p>HI, I'M A FREELANCER</p>
-        <h1>butcherthemulch.</h1>
+        <h1>iamcarterballow.</h1>
         <div>
           <Link to="/projects" className="btn">Projects</Link>
           <Link to="/contact" className="btn btn-light">Contact</Link>
