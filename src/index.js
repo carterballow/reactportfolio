cbacfe77e630ebc1029153fbb8c8ab1fd6001d78
carterballow <cartsballow@gmail.com>
2025-02-28 23:52:57 -0800

    Fix router basename for GitHub Pages deployment

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 // Make sure to replace '<repository-name>' with your actual repository name
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="/<repository-name>"> {/* Added basename for GitHub Pages */}
+  <BrowserRouter basename="/reactportfolio"> {/* Added basename for GitHub Pages */}
     <App />
   </BrowserRouter>
 );
